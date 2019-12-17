f33a32b60d3e772ece14b84a1aee2f809f87afd6
Vinod <vinod.kottamasu@upshot.ai>
2019-12-17 23:55:15 +0530

    initial commit

---
+++ DemoFramework/DemoFramework/Demo.swift
@@ -0,0 +1,18 @@
+//
+//  Demo.swift
+//  DemoFramework
+//
+//  Created by Vinod on 17/12/19.
+//  Copyright © 2019 Vinod. All rights reserved.
+//
+
+import Foundation
+
+public class Demo {
+    
+    private init() {}
+    
+    public static func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+}
---
+++ DemoFramework/DemoFramework/DemoFramework.h
@@ -0,0 +1,19 @@
+//
+//  DemoFramework.h
+//  DemoFramework
+//
+//  Created by Vinod on 17/12/19.
+//  Copyright © 2019 Vinod. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DemoFramework.
+FOUNDATION_EXPORT double DemoFrameworkVersionNumber;
+
+//! Project version string for DemoFramework.
+FOUNDATION_EXPORT const unsigned char DemoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoFramework/PublicHeader.h>
+
+
